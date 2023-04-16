15033af7583811650d3a92439edcebea7384fb06
dilbazi <you@example.com>
2023-04-16 15:34:36 +0400

    articles

--- blog/app.py
+++ blog/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 from blog.report.views import report
 from blog.user.views  import user
+from blog.article.views  import article
 def create_app()->Flask:
     app=Flask(__name__)
     register_blueprints(app)
@@ -9,3 +10,4 @@ def create_app()->Flask:
 def register_blueprints(app:Flask):
     app.register_blueprint(user)
     app.register_blueprint(report)
+    app.register_blueprint(article)
