1291cfb3d8830b5d045bea37426ed09fb8720cee
dilbazi <you@example.com>
2023-05-25 20:30:33 +0400

    swagger

--- blog/api/__init__.py
+++ blog/api/__init__.py
@@ -1,9 +1,22 @@
-from flask_combo_jsonapi import Api
 from blog.api.tag import TagList, TagDetail
+from combojsonapi.spec import ApiSpecPlugin
+from flask_combo_jsonapi import Api
 
 
+
+
+def create_api_spec_plugin(app):
+    api_spec_plugin = ApiSpecPlugin(
+        app=app,
+        tags={
+        "Tag": "Tag API",
+        }
+    )
+    return api_spec_plugin
 def init_api(app):
     api = Api(app)
+    api_spec_plugin = create_api_spec_plugin(app)
+    api = Api(app=app, plugins=[api_spec_plugin])
     api.route(TagList, "tag_list", "/api/tags/")
     api.route(TagDetail, "tag_detail", "/api/tags/<int:id>/")
     return api
