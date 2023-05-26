97af0f7a05d227321b36b26d8829d54cba4716d5
dilbazi <you@example.com>
2023-05-26 23:45:45 +0400

    article count plugin

--- blog/api/__init__.py
+++ blog/api/__init__.py
@@ -4,10 +4,12 @@ from flask_combo_jsonapi import Api
 from blog.api.article import ArticleDetail, ArticleList
 from blog.api.author import AuthorDetail, AuthorList
 from blog.api.user import UserDetail, UserList
+from combojsonapi.event import EventPlugin
 
 
 
 def create_api_spec_plugin(app):
+
     api_spec_plugin = ApiSpecPlugin(
         app=app,
         tags={
@@ -20,8 +22,10 @@ def create_api_spec_plugin(app):
     return api_spec_plugin
 def init_api(app):
     api = Api(app)
+    event_plugin = EventPlugin()
     api_spec_plugin = create_api_spec_plugin(app)
-    api = Api(app=app, plugins=[api_spec_plugin])
+    api = Api(app=app, plugins=[api_spec_plugin, event_plugin, ])
+    # api = Api(app=app, plugins=[api_spec_plugin])
     api.route(TagList, "tag_list", "/api/tags/")
     api.route(TagDetail, "tag_detail", "/api/tags/<int:id>/")
 
--- blog/api/article.py
+++ blog/api/article.py
@@ -3,9 +3,17 @@ from flask_combo_jsonapi import ResourceList, ResourceDetail
 from blog.models.database import db
 from blog.models.user import Article
 from blog.schemas import ArticleSchema
+from combojsonapi.event.resource import EventsResource
+
+
+class ArticleListEvent(EventsResource):
+
+    def event_get_count(self, *args, **kwargs):
+        return {'count': Article.query.count()}
 
 
 class ArticleList(ResourceList):
+    events = ArticleListEvent
     schema = ArticleSchema
     data_layer = {
         'session': db.session,
