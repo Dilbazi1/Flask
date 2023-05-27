448826a54c81265ec59253cd5545df132e1824a1
dilbazi <you@example.com>
2023-05-27 22:23:32 +0400

    ArticleDetailEvent

--- blog/api/article.py
+++ blog/api/article.py
@@ -11,7 +11,10 @@ class ArticleListEvent(EventsResource):
     def event_get_count(self, *args, **kwargs):
         return {'count': Article.query.count()}
 
+class ArticleDetailEvent(EventsResource):
 
+    def event_get_count_by_author(self, *args, **kwargs):
+        return {'count': Article.query.filter(Article.author_id == kwargs['id']).count()}
 class ArticleList(ResourceList):
     events = ArticleListEvent
     schema = ArticleSchema
@@ -21,7 +24,10 @@ class ArticleList(ResourceList):
     }
 
 
+
+
 class ArticleDetail(ResourceDetail):
+    events = ArticleDetailEvent
     schema = ArticleSchema
     data_layer = {
         'session': db.session,
